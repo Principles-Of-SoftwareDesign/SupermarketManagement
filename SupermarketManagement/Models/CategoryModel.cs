ede457e7c65e8398e301bc9d83061d5c649c38a6
mariamsayed1 <mariamsayed777@gmail.com>
2024-05-27 05:54:39 +0200

    category model

--- SupermarketManagement/Models/CategoryModel.cs
+++ SupermarketManagement/Models/CategoryModel.cs
@@ -8,5 +8,24 @@ namespace SupermarketManagement.Models
 {
     public class CategoryModel
     {
+        private int id;
+        private int category_id;
+        private string name;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int CategoryID
+        {
+            get { return category_id; }
+            set { category_id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
     }
 }
