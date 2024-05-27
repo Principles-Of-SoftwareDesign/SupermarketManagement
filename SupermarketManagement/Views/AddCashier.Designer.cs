c1722246e7c4a9df727ab2c977e1d77287ea7394
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-05-28 00:39:32 +0300

    .

--- SupermarketManagement/Views/AddCashier.Designer.cs
+++ SupermarketManagement/Views/AddCashier.Designer.cs
@@ -51,35 +51,39 @@
             AdminNavbar.Controls.Add(adding);
             AdminNavbar.Dock = DockStyle.Top;
             AdminNavbar.Location = new Point(0, 0);
+            AdminNavbar.Margin = new Padding(4, 4, 4, 4);
             AdminNavbar.Name = "AdminNavbar";
             AdminNavbar.SelectedIndex = 0;
-            AdminNavbar.Size = new Size(1315, 47);
+            AdminNavbar.Size = new Size(1644, 59);
             AdminNavbar.TabIndex = 22;
             // 
             // home
             // 
-            home.Location = new Point(4, 29);
+            home.Location = new Point(4, 34);
+            home.Margin = new Padding(4, 4, 4, 4);
             home.Name = "home";
-            home.Padding = new Padding(3);
-            home.Size = new Size(1307, 14);
+            home.Padding = new Padding(4, 4, 4, 4);
+            home.Size = new Size(1636, 21);
             home.TabIndex = 1;
             home.Text = "Home";
             home.UseVisualStyleBackColor = true;
             // 
             // ViewAdmins
             // 
-            ViewAdmins.Location = new Point(4, 29);
+            ViewAdmins.Location = new Point(4, 34);
+            ViewAdmins.Margin = new Padding(4, 4, 4, 4);
             ViewAdmins.Name = "ViewAdmins";
-            ViewAdmins.Size = new Size(1307, 14);
+            ViewAdmins.Size = new Size(1636, 21);
             ViewAdmins.TabIndex = 2;
             ViewAdmins.Text = "View Admins";
             ViewAdmins.UseVisualStyleBackColor = true;
             // 
             // adding
             // 
-            adding.Location = new Point(4, 29);
+            adding.Location = new Point(4, 34);
+            adding.Margin = new Padding(4, 4, 4, 4);
             adding.Name = "adding";
-            adding.Size = new Size(1307, 14);
+            adding.Size = new Size(1636, 21);
             adding.TabIndex = 3;
             adding.Text = "Add Admin";
             adding.UseVisualStyleBackColor = true;
@@ -88,9 +92,10 @@
             // 
             addAdminBtn.BackColor = SystemColors.HotTrack;
             addAdminBtn.ForeColor = Color.White;
-            addAdminBtn.Location = new Point(524, 498);
+            addAdminBtn.Location = new Point(655, 623);
+            addAdminBtn.Margin = new Padding(4, 4, 4, 4);
             addAdminBtn.Name = "addAdminBtn";
-            addAdminBtn.Size = new Size(233, 48);
+            addAdminBtn.Size = new Size(291, 60);
             addAdminBtn.TabIndex = 21;
             addAdminBtn.Text = "Add New Cashier";
             addAdminBtn.UseVisualStyleBackColor = false;
@@ -98,77 +103,81 @@
             // 
             // passwordTxt
             // 
-            passwordTxt.Location = new Point(538, 419);
+            passwordTxt.Location = new Point(672, 524);
+            passwordTxt.Margin = new Padding(4, 4, 4, 4);
             passwordTxt.Name = "passwordTxt";
-            passwordTxt.Size = new Size(327, 27);
+            passwordTxt.Size = new Size(408, 31);
             passwordTxt.TabIndex = 20;
             // 
             // passwordLabel
             // 
             passwordLabel.AutoSize = true;
-            passwordLabel.Location = new Point(364, 419);
-            passwordLabel.Margin = new Padding(4, 0, 4, 0);
+            passwordLabel.Location = new Point(455, 524);
+            passwordLabel.Margin = new Padding(5, 0, 5, 0);
             passwordLabel.Name = "passwordLabel";
-            passwordLabel.Size = new Size(70, 20);
+            passwordLabel.Size = new Size(87, 25);
             passwordLabel.TabIndex = 19;
             passwordLabel.Text = "Password";
             // 
             // numberTxt
             // 
-            numberTxt.Location = new Point(538, 329);
+            numberTxt.Location = new Point(672, 411);
+            numberTxt.Margin = new Padding(4, 4, 4, 4);
             numberTxt.Name = "numberTxt";
-            numberTxt.Size = new Size(327, 27);
+            numberTxt.Size = new Size(408, 31);
             numberTxt.TabIndex = 18;
             // 
             // numberLabel
             // 
             numberLabel.AutoSize = true;
-            numberLabel.Location = new Point(364, 329);
-            numberLabel.Margin = new Padding(4, 0, 4, 0);
+            numberLabel.Location = new Point(455, 411);
+            numberLabel.Margin = new Padding(5, 0, 5, 0);
             numberLabel.Name = "numberLabel";
-            numberLabel.Size = new Size(108, 20);
+            numberLabel.Size = new Size(132, 25);
             numberLabel.TabIndex = 17;
             numberLabel.Text = "Phone Number";
             // 
             // emailTxt
             // 
-            emailTxt.Location = new Point(538, 254);
+            emailTxt.Location = new Point(672, 318);
+            emailTxt.Margin = new Padding(4, 4, 4, 4);
             emailTxt.Name = "emailTxt";
-            emailTxt.Size = new Size(327, 27);
+            emailTxt.Size = new Size(408, 31);
             emailTxt.TabIndex = 16;
             // 
             // emailLabel
             // 
             emailLabel.AutoSize = true;
-            emailLabel.Location = new Point(364, 254);
-            emailLabel.Margin = new Padding(4, 0, 4, 0);
+            emailLabel.Location = new Point(455, 318);
+            emailLabel.Margin = new Padding(5, 0, 5, 0);
             emailLabel.Name = "emailLabel";
-            emailLabel.Size = new Size(46, 20);
+            emailLabel.Size = new Size(54, 25);
             emailLabel.TabIndex = 15;
             emailLabel.Text = "Email";
             // 
             // nameTxt
             // 
-            nameTxt.Location = new Point(538, 176);
+            nameTxt.Location = new Point(672, 220);
+            nameTxt.Margin = new Padding(4, 4, 4, 4);
             nameTxt.Name = "nameTxt";
-            nameTxt.Size = new Size(327, 27);
+            nameTxt.Size = new Size(408, 31);
             nameTxt.TabIndex = 14;
             // 
             // nameLabel
             // 
             nameLabel.AutoSize = true;
-            nameLabel.Location = new Point(364, 176);
-            nameLabel.Margin = new Padding(4, 0, 4, 0);
+            nameLabel.Location = new Point(455, 220);
+            nameLabel.Margin = new Padding(5, 0, 5, 0);
             nameLabel.Name = "nameLabel";
-            nameLabel.Size = new Size(49, 20);
+            nameLabel.Size = new Size(59, 25);
             nameLabel.TabIndex = 13;
             nameLabel.Text = "Name";
             // 
             // AddCashier
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1315, 579);
+            ClientSize = new Size(1644, 724);
             Controls.Add(AdminNavbar);
             Controls.Add(addAdminBtn);
             Controls.Add(passwordTxt);
@@ -179,7 +188,8 @@
             Controls.Add(emailLabel);
             Controls.Add(nameTxt);
             Controls.Add(nameLabel);
-           
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "AddCashier";
             Text = "AddCashier";
             AdminNavbar.ResumeLayout(false);
             ResumeLayout(false);
