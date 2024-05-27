35f30b3a5e58dd6f9281dd64e05b1f6fbbaf16e1
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2024-05-28 01:13:06 +0300

    Login MVP

---
+++ SupermarketManagement/Views/ILoginView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketManagement.Views
+{
+    public interface ILoginView
+    {
+        string Email { get; }
+        string Password { get; }
+        void ShowMessage(string message);
+        void OpenAdminView();
+        void OpenCategoryView();
+        void ClearForm();
+    }
+}
--- SupermarketManagement/Views/login.Designer.cs
+++ SupermarketManagement/Views/login.Designer.cs
@@ -109,7 +109,6 @@
             txtpassword.Name = "txtpassword";
             txtpassword.Size = new Size(297, 24);
             txtpassword.TabIndex = 12;
-            txtpassword.TextChanged += txtpassword_TextChanged;
             // 
             // txtUserName
             // 
@@ -146,7 +145,6 @@
             Controls.Add(pictureBox1);
             Name = "login";
             Text = "login";
-            Load += this.login_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
