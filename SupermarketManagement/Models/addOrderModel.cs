b6c60d51326412e45250c8800b389873ae368e48
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-05-28 01:02:11 +0300

    Add order model

---
+++ SupermarketManagement/Models/addOrderModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketManagement.Models
+{
+    public class addOrderModel
+    {
+        private int id;
+        private string name;
+        private double price;
+        public addOrderModel()
+        {
+            id = -1;
+            name = "";
+            price = 0.0;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+
+
+        }
+    }
+}
