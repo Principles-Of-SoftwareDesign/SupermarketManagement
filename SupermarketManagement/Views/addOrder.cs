9b2d8c17114143943d9f8b4e602042ad106fe219
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-05-28 00:54:10 +0300

    add order design

---
+++ SupermarketManagement/Views/addOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupermarketManagement.Views
+{
+    public partial class addOrder : Form
+    {
+        public addOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void addAdminBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
